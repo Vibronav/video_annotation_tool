e063fdf4bb1a96bdfebb08d7a1b42ecfa390e55f
Gawron97 <jakubgawron97@gmail.com>
2025-09-21 10:56:30 +0200

    chage requirements encoding

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as fp:
-    install_requires = fp.read()
+try:
+    with open('requirements.txt', encoding='utf-16') as fp:
+        install_requires = fp.read()
+except:
+    with open('requirements.txt') as fp:
+        install_requires = fp.read()
 
 setup(
     name='video-annotation-tool',
